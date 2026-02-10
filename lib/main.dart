8f8baa9c1464e64ecd650bafba2ef41f0abf89bf
kimilguk <kimilguk19@gmail.com>
2026-02-10 17:49:54 +0900

    13주차 3교시 강의 소스

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class Task { //* 회원조회에 사용할 description 필드 추가예정.
   // API 응답(JSON)을 Task 객체로 변환하는 factory 생성자
   factory Task.fromJson(Map<String, dynamic> json) {
     return Task(
-      id: json['id'] as dynamic, // API 필드명에 맞게 수정 json['_id']
+      id: json['_id']??json['id'] as dynamic, // API 필드명에 맞게 수정
       title: json['title'] as String,
       status: json['status'] as String? ?? 'Not Started', // API 필드명 및 기본값 설정
       description: json['description'] as dynamic,
@@ -46,7 +46,7 @@ class Task { //* 회원조회에 사용할 description 필드 추가예정.
   // Task 객체를 JSON으로 변환하는 메서드 (POST, PUT 요청 시 사용)
   Map<String, dynamic> toJson() {
     return {
-      'id': id, // '_id':id는 서버에서 생성될 경우 보내지 않을 수 있음
+      //'_id': id, // id는 서버에서 자동생성되기때문에 주석처리
       'title': title,
       'status': status,
       'description': description,
